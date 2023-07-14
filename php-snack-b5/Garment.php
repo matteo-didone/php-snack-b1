87c32687af92585eeeef9047264ac928a3b37b62
matteo-didone <matteo.dido9@gmail.com>
2023-07-14 12:37:35 +0200

    Added basic files, created Garment class, declared material variable, used "material" property to populate constructor

---
+++ php-snack-b5/Garment.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Garment {
+    public String $material; 
+
+    public function __construct($material) {
+        $this->material = $material;
+    }
+
+}
+?>
---
+++ php-snack-b5/index.php
@@ -0,0 +1,9 @@
+<!-- 
+Crea una classe Indumento che ha un materiale come proprieta' da popolare nel costruttore.
+
+Crea due sottoclassi Scarpa e Cappello, che avranno una proprieta' speciale ognuna 
+oltre a richiamare il costruttore del parent. 
+
+Istanziare un indumento, una scarpa e un cappello con il var_dump e verificare 
+su schermo le differenze.
+-->
